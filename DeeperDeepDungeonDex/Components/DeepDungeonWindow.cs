27f31a51e6ce241eb0bb432351e21ba766041eba
MidoriKami <9083275+MidoriKami@users.noreply.github.com>
2024-01-11 19:10:16 -0800

    Add base class for all windows.

---
+++ DeeperDeepDungeonDex/Components/DeepDungeonWindow.cs
@@ -0,0 +1,31 @@
+﻿using Dalamud.Interface.Windowing;
+
+namespace DeeperDeepDungeonDex.System;
+
+/// <summary>
+/// This class is an extension of a Dalamud Window, to allow for easier collapse control.
+/// </summary>
+public abstract class DeepDungeonWindow(string name) : Window(name) {
+    private bool isCollapsed;
+
+    public void UnCollapseOrShow() {
+        TryUnCollapse();
+        IsOpen = true;
+    }
+
+    public void UnCollapseOrToggle() {
+        TryUnCollapse();
+        Toggle();
+    }
+
+    private void TryUnCollapse() {
+        if (isCollapsed) {
+            UnCollapse();
+        }
+    }
+    
+    private void UnCollapse() {
+        isCollapsed = false;
+        Collapsed = false;
+    }
+}
