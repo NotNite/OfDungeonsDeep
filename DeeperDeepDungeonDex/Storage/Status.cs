d713a509c83b39daf765590c3bf23699c3f01ed3
MidoriKami <9083275+MidoriKami@users.noreply.github.com>
2024-01-12 21:42:32 -0800

    Assign statuses to icon id for said status

--- DeeperDeepDungeonDex/Storage/Status.cs
+++ DeeperDeepDungeonDex/Storage/Status.cs
@@ -3,11 +3,11 @@
 namespace DeeperDeepDungeonDex.Storage;
 
 [JsonConverter(typeof(JsonStringEnumConverter))]
-public enum Status {
-    Bind,
-    Heavy,
-    Sleep,
-    Slow,
-    Stun,
-    Resolution
+public enum Status : uint {
+    Bind = 15003,
+    Heavy = 15002,
+    Sleep = 15013,
+    Slow = 15009,
+    Stun = 15004,
+    Resolution = 0,
 }
