c659cb3f4061504289928f60345a327b4dba9cb2
MidoriKami <9083275+MidoriKami@users.noreply.github.com>
2024-01-12 21:42:57 -0800

    Add ITextureProvider Service

--- DeeperDeepDungeonDex/Services.cs
+++ DeeperDeepDungeonDex/Services.cs
@@ -14,4 +14,5 @@ public class Services {
     [PluginService] public static IClientState ClientState { get; private set; } = null!;
     [PluginService] public static IDutyState DutyState { get; private set; } = null!;
     [PluginService] public static IDataManager DataManager { get; private set; } = null!;
+    [PluginService] public static ITextureProvider TextureProvider { get; private set; } = null!;
 }
