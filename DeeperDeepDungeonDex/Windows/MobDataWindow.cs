b8ced61fca1845762b5e4423dc144d8f3d73f57e
MidoriKami <9083275+MidoriKami@users.noreply.github.com>
2024-01-13 16:37:51 -0800

    Add remaining data fields to mob data window

--- DeeperDeepDungeonDex/Windows/MobDataWindow.cs
+++ DeeperDeepDungeonDex/Windows/MobDataWindow.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Numerics;
 using System.Reflection;
 using Dalamud.Interface;
+using Dalamud.Interface.Components;
 using Dalamud.Interface.Internal;
 using Dalamud.Interface.Utility;
 using DeeperDeepDungeonDex.Storage;
@@ -15,30 +16,31 @@ namespace DeeperDeepDungeonDex.System;
 
 public class MobDataWindow : DeepDungeonWindow {
     public Enemy Enemy { get; }
-    private IDalamudTextureWrap? mobImageSmall;
-    private IDalamudTextureWrap? mobImageLarge;
+    private readonly IDalamudTextureWrap? mobImageSmall;
+    private readonly IDalamudTextureWrap? mobImageLarge;
 
     public MobDataWindow(string name, Enemy enemy) : base(name) {
         Enemy = enemy;
         
         SizeConstraints = new WindowSizeConstraints {
-            MinimumSize = new Vector2(300.0f, 200.0f),
-            MaximumSize = new Vector2(float.PositiveInfinity),
+            MinimumSize = new Vector2(325.0f, 185.0f),
+            MaximumSize = new Vector2(325.0f, 185.0f),
         };
 
-        if (Enemy.Image is not null) {
-            var assemblyDir = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location)!;
+        Flags |= ImGuiWindowFlags.NoResize;
+        Flags |= ImGuiWindowFlags.NoTitleBar;
 
-            mobImageLarge = Services.TextureProvider.GetTextureFromFile(new FileInfo(GetImagePath(assemblyDir, "Images")));
-            mobImageSmall = Services.TextureProvider.GetTextureFromFile(new FileInfo( GetImagePath(assemblyDir, "Thumbnails")));
+        if (Enemy.Image is not null) {
+            mobImageLarge = Services.TextureProvider.GetTextureFromFile(new FileInfo(GetImagePath("Images")));
+            mobImageSmall = Services.TextureProvider.GetTextureFromFile(new FileInfo( GetImagePath("Thumbnails")));
         }
     }
     
-    private string GetImagePath(string assemblyDir, string folder) {
+    private string GetImagePath(string folder) {
         if (Enemy.Image is null) return string.Empty;
         
         return Path.Combine(
-            assemblyDir,
+            Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location)!,
             "Data",
             folder,
             Plugin.GetDeepDungeonType() switch {
@@ -55,8 +57,7 @@ public class MobDataWindow : DeepDungeonWindow {
     public override void Draw() {
         base.Draw();
         
-        const float portraitHeight = 75.0f;
-        
+        const float portraitHeight = 85.0f;
         var portraitSize = new Vector2(ImGui.GetContentRegionMax().X * 0.25f, portraitHeight * ImGuiHelpers.GlobalScale);
         if (ImGui.BeginChild("MobPortrait", portraitSize, false)) {
             DrawPortrait();
@@ -66,16 +67,30 @@ public class MobDataWindow : DeepDungeonWindow {
         ImGui.SameLine();
         var basicInfoSize = new Vector2(ImGui.GetContentRegionAvail().X, portraitHeight * ImGuiHelpers.GlobalScale);
         if (ImGui.BeginChild("BasicInfo", basicInfoSize, false)) {
-            var nameSize = new Vector2(ImGui.GetContentRegionAvail().X, ImGui.GetTextLineHeight());
-            if (ImGui.BeginChild("MobName", nameSize, false)) {
-                DrawMobName();
+            if (ImGui.BeginTable("NameCloseTable", 2)) {
+                ImGui.TableSetupColumn("##Name", ImGuiTableColumnFlags.WidthStretch);
+                ImGui.TableSetupColumn("##Close", ImGuiTableColumnFlags.WidthFixed, 20.0f * ImGuiHelpers.GlobalScale);
+
+                ImGui.TableNextColumn();
+                var nameSize = new Vector2(ImGui.GetContentRegionAvail().X, ImGui.GetTextLineHeight() + 5.0f * ImGuiHelpers.GlobalScale);
+                if (ImGui.BeginChild("MobName", nameSize, false, ImGuiWindowFlags.NoScrollWithMouse | ImGuiWindowFlags.NoScrollbar)) {
+                    ImGui.AlignTextToFramePadding();
+                    DrawMobName();
+                }
+                ImGui.EndChild();
+
+                ImGui.TableNextColumn();
+                if (ImGuiComponents.IconButton("Close", FontAwesomeIcon.Times)) {
+                    OnClose();
+                }
+                
+                ImGui.EndTable();
             }
-            ImGui.EndChild();
 
             var mobIdSize = new Vector2(ImGui.GetContentRegionAvail().X, ImGui.GetTextLineHeight());
             ImGui.SetCursorPosX(ImGui.GetContentRegionAvail().X - mobIdSize.X);
-            if (ImGui.BeginChild("MobId", mobIdSize, false)) {
-                DrawMobHpAndId();
+            if (ImGui.BeginChild("MobId", mobIdSize, false, ImGuiWindowFlags.NoScrollWithMouse)) {
+                DrawBasicMobData();
             }
             ImGui.EndChild();
 
@@ -87,30 +102,45 @@ public class MobDataWindow : DeepDungeonWindow {
         }
         ImGui.EndChild();
 
-        var mobDataSize = new Vector2(ImGui.GetContentRegionAvail().X, 100.0f);
-        if (ImGui.BeginChild("MobData", mobDataSize, true)) {
-            
+        ImGuiHelpers.ScaledDummy(5.0f);
+        
+        var mobDataSize = new Vector2(ImGui.GetContentRegionAvail().X, 65.0f * ImGuiHelpers.GlobalScale);
+        if (ImGui.BeginChild("MobData", mobDataSize, false)) {
+            DrawMobData();
         }
         ImGui.EndChild();
-
-        //
-        // ImGui.TextUnformatted($"Name: {Plugin.GetEnemyName(Enemy)}");
-        // ImGui.TextUnformatted($"Aggro: {this.Enemy.Aggro}");
-        //
-        // if (this.Enemy.AttackType is not null)
-        //     ImGui.TextUnformatted($"Attack type: {this.Enemy.AttackType}");
-        //
-        // var vulnerabilities = this.Enemy.Vulnerabilities
-        //     .Where(x => x.Value)
-        //     .Select(x => x.Key);
-        //
-        // ImGui.TextUnformatted($"Weakness: {string.Join(", ", vulnerabilities)}");
     }
     
+    private void DrawMobData() {
+        if (ImGui.BeginTable("DataTable", 2, ImGuiTableFlags.SizingStretchProp)) {
+            ImGui.TableNextColumn();
+            ImGui.Text("Floors");
+
+            ImGui.TableNextColumn();
+            ImGui.Text(string.Join(", ",  Enumerable.Range(Enemy.StartFloor, Enemy.EndFloor - Enemy.StartFloor + 1)));
+
+            ImGui.TableNextColumn();
+            ImGui.Text("Aggro");
+
+            ImGui.TableNextColumn();
+            ImGui.Text(Enemy.Aggro.ToString());
+
+            if (Enemy.AttackName is not null) {
+                ImGui.TableNextColumn();
+                ImGui.Text("Attack");
+
+                ImGui.TableNextColumn();
+                ImGui.Text(Enemy.AttackName);
+            }
+            
+            ImGui.EndTable();
+        }
+    }
+
     private void DrawVulnerabilities() {
         foreach (var (status, isVulnerable) in Enemy.Vulnerabilities) {
             if (Services.TextureProvider.GetIcon((uint) status) is { } image) {
-                ImGui.Image(image.ImGuiHandle, image.Size * ImGuiHelpers.GlobalScale * 0.50f, Vector2.Zero, Vector2.One, isVulnerable ? Vector4.One : Vector4.One with { W = 0.33f });
+                ImGui.Image(image.ImGuiHandle, image.Size * ImGuiHelpers.GlobalScale * 0.50f, Vector2.Zero, Vector2.One, isVulnerable ? Vector4.One : Vector4.One / 2.5f );
 
                 if (ImGui.IsItemHovered() && Services.DataManager.GetExcelSheet<Status>()?.FirstOrDefault(statusEffect => statusEffect.Icon == (uint)status) is {} statusInfo ) {
                     ImGui.SetTooltip(statusInfo.Name);
@@ -120,14 +150,22 @@ public class MobDataWindow : DeepDungeonWindow {
         }
     }
     
-    private void DrawMobHpAndId() {
-        ImGui.Text($"HP {Enemy.Hp:n0}");
-        ImGui.SameLine();
-        
-        var mobId = $"Id {Enemy.Id.ToString()}";
-        var mobIdTextSize = ImGui.CalcTextSize(mobId);
-        ImGui.SetCursorPosX(ImGui.GetContentRegionMax().X - mobIdTextSize.X);
-        ImGui.Text(mobId);
+    private void DrawBasicMobData() {
+        if (ImGui.BeginTable("BasicInfoTable", 3, ImGuiTableFlags.SizingStretchSame, ImGui.GetContentRegionAvail())) {
+            ImGui.TableNextColumn();
+            ImGui.Text($"{Enemy.Family ?? string.Empty}");
+
+            ImGui.TableNextColumn();
+            ImGui.Text($"HP {Enemy.Hp ?? 0}");
+            
+            ImGui.TableNextColumn();
+            var idText = $"ID {Enemy.Id}";
+            var idTextSize = ImGui.CalcTextSize(idText);
+            ImGui.SetCursorPosX(ImGui.GetContentRegionMax().X - idTextSize.X);
+            ImGui.Text(idText);
+            
+            ImGui.EndTable();
+        }
     }
 
     private void DrawPortrait() {
